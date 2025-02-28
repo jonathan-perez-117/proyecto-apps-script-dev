519a7d08041f1c4b8943b41740b876b25e81ad07
jonathan-perez-117 <l21070077@valladolid.tecnm.mx>
2025-02-27 18:24:22 -0600

    Actualizar Datos

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -20,3 +20,19 @@ function guardarUsuario(usuario){
     function listarUsuarios(id = undefined){
         return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIOS), id));
     }
+
+    function actualizarUsuario(id, datos) {
+        try {
+          const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIOS);
+          Update(id, datos, sheetUsuarios);
+          return {
+            titulo: "Actualizado correctamente",
+            descripcion: "Ya se encuentra el usuario actualizado en la base de datos.",
+          };
+        } catch (error) {
+          return {
+            titulo: "Ops ha ocurrido un error! " + error,
+            descripcion: "Por favor contacte a soporte.",
+          };
+        }
+      }
